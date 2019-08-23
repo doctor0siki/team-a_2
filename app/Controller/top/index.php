bd2cd70aec2d75550c56166c5325edf1b80a50f0
y-fukumoto <y-fukumoto@ceres-inc.jp>
2019-08-23 14:05:27 +0900

    add more function

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -8,6 +8,20 @@ $app->get('/', function (Request $request, Response $response) {
 
     $data = [];
 
+    //プルダウンに漢数字を出すために定義をする
+    $data["kanji_num"] = array(
+        1  => "壱",
+        2  => "弐",
+        3  => "参",
+        4  => "四",
+        5  => "伍",
+        6  => "陸",
+        7  => "漆",
+        8  => "捌",
+        9  => "玖",
+        10 => "拾"
+    );
+
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
 });
